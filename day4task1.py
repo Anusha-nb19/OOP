4951d04199326d3fd7f0f550b02cc74db165e37c
Anusha-nb19 <laughanusha@gmail.com>
2026-02-05 22:46:24 +0530

    added task1 of day4

---
+++ day4task1.py
@@ -0,0 +1,54 @@
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited Amount: ₹", amount)
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print("Withdrawn Amount: ₹", amount)
+        else:
+            print("Insufficient balance")
+
+    def display_balance(self):
+        print("Account Holder:", self.account_holder)
+        print("Current Balance: ₹", self.balance)
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, account_holder, balance, interest_rate):
+        super().__init__(account_holder, balance)
+        self.interest_rate = interest_rate
+
+    def add_interest(self):
+        interest = self.balance * self.interest_rate / 100
+        self.balance += interest
+        print("Interest Added: ₹", interest)
+
+
+class CurrentAccount(BankAccount):
+    def __init__(self, account_holder, balance, overdraft_limit):
+        super().__init__(account_holder, balance)
+        self.overdraft_limit = overdraft_limit
+
+    def withdraw_with_overdraft(self, amount):
+        if amount <= self.balance + self.overdraft_limit:
+            self.balance -= amount
+            print("Withdrawn with Overdraft: ₹", amount)
+        else:
+            print("Overdraft limit exceeded")
+
+
+savings = SavingsAccount("Anusha", 5000, 5)
+savings.deposit(2000)
+savings.add_interest()
+savings.withdraw(3000)
+savings.display_balance()
+
+current = CurrentAccount("Anusha", 3000, 2000)
+current.withdraw_with_overdraft(4500)
+current.display_balance()
